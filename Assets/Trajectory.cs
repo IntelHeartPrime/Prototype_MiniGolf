4a315cfd4bf0c92d7dc26d6d5427fa1a2428bb73
yanxin <intelheartprime@outlook.com>
2022-11-26 22:12:46 +0800

    Update project

---
+++ Assets/Trajectory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour
+{   
+    // Draw trajectory in Editor & playing mode 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
